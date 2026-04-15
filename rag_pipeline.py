1e6107ffdfc6a5a0dd86ae7b355a2f89f4ab32ec
Devansh Thakre <100189235+DevanshThakre07@users.noreply.github.com>
2026-04-15 14:50:09 +0530

    quick fix

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,27 +1,18 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-# from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFaceEmbeddings
 import os
-# import google.generativeai as genai
 from google import genai 
 
-# 🔹 INIT ONCE
 embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 
 key = os.getenv('GOOGLE_API_KEY')
-# change
-# genai.configure(api_key= key)
 client = genai.Client(api_key= key)
-# llm = genai.client.models('gemini-2.5-flash-lite')
 model_id = 'gemini-2.5-flash-lite'
-# genai.configure(api_key="AIzaSyBcOaSjnxRdRFoFtOmecXNIyALZnFa2rOA")# give api key
-# llm = genai.GenerativeModel('gemini-2.5-flash-lite')
 
 
-#  LOAD DOCUMENTS (STATIC)
 def initialize_vector_store(file_paths):
     global vector_store
 
@@ -81,19 +72,11 @@ def generate_answer(query, context):
     {query}
     """
 
-    # response = llm.generate_content(prompt)
     response = client.models.generate_content(
     model=model_id,
     contents=prompt)
     return response.text
 
-# Agentic decision
-def agentic_decision(query:str):
-    query = query.lower()
-    if 'document' in query or 'pdf' in query:
-        return 'rag'
-    else:
-        return 'llm'
 
 # MAIN FUNCTION
 def answer_query(query: str):
@@ -101,8 +84,6 @@ def answer_query(query: str):
 
     #  Step 1: If no document → LLM
     if vector_store is None:
-        # change
-        # response = llm.generate_content(query)
         response = client.models.generate_content(
         model=model_id,
         contents=query)
@@ -113,9 +94,9 @@ def answer_query(query: str):
 
     #  Step 3: If weak retrieval → fallback
     if len(docs) == 0:
-        # chenge
-        # response = llm.generate_content(query)
-
+        response = client.models.generate_content(
+        model=model_id,
+        contents=query)
         return {"answer": response.text, "sources": []}
 
     #  Step 4: Use RAG
